967ba7879b9d31a274946e6065ad62cd6844342f
Jordan Sim-Smith <jordansimsmith@gmail.com>
2020-02-08 10:20:23 +1300

    Created basic homescreen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, StatusBar } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
-import { HomeScreen } from './src/screens/HomeScreen';
+import HomeScreen from './src/screens/HomeScreen';
 
 export default function Main() {
   return (
     <PaperProvider>
+      <StatusBar barStyle="light-content" />
       <HomeScreen />
     </PaperProvider>
   );
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,71 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Title, Button } from 'react-native-paper';
+import React, { FunctionComponent } from 'react';
+import { View, TouchableOpacity, StyleSheet } from 'react-native';
+import { Title, Button, Theme, withTheme } from 'react-native-paper';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-export const HomeScreen = props => {
+interface IProps {
+  theme: Theme;
+}
+
+const HomeScreen: FunctionComponent<IProps> = (props: IProps) => {
+  const { theme } = props;
+
   return (
-    <View>
-      <Title>500 Scorer</Title>
-      <Button mode="contained">New Game</Button>
+    <View
+      style={{
+        ...styles.container,
+        backgroundColor: theme.colors.primary,
+      }}
+    >
+      <View style={styles.headerContainer}>
+        <MaterialCommunityIcons
+          name="cards-spade"
+          size={150}
+          color={theme.colors.surface}
+        />
+        <Title style={{ ...styles.header, color: theme.colors.surface }}>
+          500 SCORER
+        </Title>
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity>
+          <Button
+            style={styles.button}
+            contentStyle={styles.buttonContent}
+            mode="contained"
+            color={theme.colors.surface}
+          >
+            New Game
+          </Button>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 20,
+  },
+  headerContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexGrow: 1,
+  },
+  header: {
+    fontSize: 30,
+    marginTop: 20,
+  },
+  buttonContainer: {
+    padding: 40,
+  },
+  button: {
+    marginBottom: 20,
+  },
+  buttonContent: {
+    height: 50,
+  },
+});
+
+export default withTheme(HomeScreen);
