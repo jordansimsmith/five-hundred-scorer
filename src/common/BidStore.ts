8d75a7b6028acb315c10b84c39a0fea74ee1f5eb
Jordan Sim-Smith <jordansimsmith@gmail.com>
2020-02-09 10:25:48 +1300

    Added fields to bid interface

--- src/common/BidStore.ts
+++ src/common/BidStore.ts
@@ -1,5 +1,12 @@
+import { Suit, Team, BidAmount } from '../enums/enums';
+
 export interface IBid {
   id: number;
+  team: Team;
+  suit: Suit;
+  amount: BidAmount;
+  teamOneTricks: number;
+  teamTwoTricks: number;
 }
 
 export interface IBidStore {
--- src/enums/enums.ts
+++ src/enums/enums.ts
@@ -1,9 +1,22 @@
 export enum Suit {
-  Spades,
-  Clubs,
-  Diamonds,
-  Hearts,
-  NoTrumps,
-  ClosedMisere,
-  OpenMisere,
+  Spades = 'S',
+  Clubs = 'C',
+  Diamonds = 'D',
+  Hearts = 'H',
+  NoTrumps = 'NT',
+  ClosedMisere = 'CM',
+  OpenMisere = 'OM',
+}
+
+export enum Team {
+  One = 1,
+  Two = 2,
+}
+
+export enum BidAmount {
+  Six = 6,
+  Seven = 7,
+  Eight = 8,
+  Nine = 9,
+  Ten = 10,
 }
