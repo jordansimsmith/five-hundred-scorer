311a95e1f92c93ff345bcbddee44fe8a186c9210
Jordan Sim-Smith <jordansimsmith@gmail.com>
2020-02-08 13:41:28 +1300

    Configured useState hook for input on new game screen

--- src/screens/NewGameScreen.tsx
+++ src/screens/NewGameScreen.tsx
@@ -1,15 +1,65 @@
-import React, { FunctionComponent } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Theme, withTheme } from 'react-native-paper';
+import React, { useState, FunctionComponent } from 'react';
+import { View, StyleSheet } from 'react-native';
+import {
+  Button,
+  Subheading,
+  TextInput,
+  Theme,
+  withTheme,
+} from 'react-native-paper';
 
 interface IProps {
   theme: Theme;
 }
 
+interface IFormData {
+  teamOnePlayerOne: string;
+  teamOnePlayerTwo: string;
+  teamTwoPlayerOne: string;
+  teamTwoPlayerTwo: string;
+}
+
+interface ITextChangeEvent {
+  name: string;
+  value: string;
+}
+
 const NewGameScreen: FunctionComponent<IProps> = (props: IProps) => {
+  const initialState: IFormData = {
+    teamOnePlayerOne: '',
+    teamOnePlayerTwo: '',
+    teamTwoPlayerOne: '',
+    teamTwoPlayerTwo: '',
+  };
+
+  const [input, setInput] = useState<IFormData>(initialState);
+
+  const handleInputChange = (e: ITextChangeEvent) => {
+    setInput({
+      ...input,
+      [e.name]: e.value,
+    });
+  };
+
   return (
     <View>
-      <Text>Welcome to the new game screen.</Text>
+      <Subheading>Team 1</Subheading>
+      <TextInput
+        onChangeText={(text: string) =>
+          handleInputChange({ name: 'teamOnePlayerOne', value: text })
+        }
+        value={input.teamOnePlayerOne}
+        mode="outlined"
+        placeholder="Player one"
+        label="Player one"
+      />
+      <TextInput />
+
+      <Subheading>Team 1</Subheading>
+      <TextInput />
+      <TextInput />
+
+      <Button mode="contained">Start</Button>
     </View>
   );
 };
