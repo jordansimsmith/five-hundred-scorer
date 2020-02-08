a6f89c48580e7ada8c5f30d006ead265e0f3a1ac
Jordan Sim-Smith <jordansimsmith@gmail.com>
2020-02-09 11:02:04 +1300

    Configured state for new bid screen for result input

--- src/screens/NewBidScreen.tsx
+++ src/screens/NewBidScreen.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, { useState, FunctionComponent } from 'react';
 import { View, TouchableOpacity, StyleSheet } from 'react-native';
 import {
   Button,
@@ -9,6 +9,8 @@ import {
   Theme,
   withTheme,
 } from 'react-native-paper';
+import { IBid } from '../common/BidStore';
+import { Suit, Team, BidAmount } from '../enums/enums';
 
 interface IProps {
   theme: Theme;
@@ -17,6 +19,34 @@ interface IProps {
 
 const NewBidScreen: FunctionComponent<IProps> = (props: IProps) => {
   const { navigation } = props;
+  const emptyBid: IBid = {
+    id: -1,
+    team: Team.One,
+    suit: Suit.Spades,
+    amount: BidAmount.Six,
+    teamOneTricks: 0,
+    teamTwoTricks: 0,
+  };
+  const [bid, setBid] = useState<IBid>(emptyBid);
+
+  const validateTricks = (): boolean => {
+    // result is valid if totalTricks is 0 (no result) or 10
+    const totalTricks: number = bid.teamOneTricks + bid.teamTwoTricks;
+    return totalTricks === 0 || totalTricks === 10;
+  };
+
+  const handleSubmit = () => {
+    if (!validateTricks(bid)) {
+      return;
+    }
+
+    // success, TODO: save to bid store
+
+    // TODO: clear fields
+
+    // return to game page
+    navigation.goBack();
+  };
 
   return (
     <View style={styles.container}>
@@ -46,18 +76,32 @@ const NewBidScreen: FunctionComponent<IProps> = (props: IProps) => {
           label="Team 1 tricks"
           keyboardType="number-pad"
           returnKeyType="done"
+          value={bid.teamOneTricks.toString()}
+          onChangeText={(tricks: string) =>
+            setBid({ ...bid, teamOneTricks: Number(tricks) })
+          }
         />
         <TextInput
           mode="outlined"
           label="Team 2 tricks"
           keyboardType="number-pad"
           returnKeyType="done"
+          value={bid.teamTwoTricks.toString()}
+          onChangeText={(tricks: string) =>
+            setBid({ ...bid, teamTwoTricks: Number(tricks) })
+          }
         />
-        <HelperText type="error">Tricks do not total 10</HelperText>
+        <HelperText type="error" visible={!validateTricks()}>
+          Tricks do not total 10
+        </HelperText>
       </Surface>
 
       <TouchableOpacity>
-        <Button style={styles.submitButton} mode="contained">
+        <Button
+          style={styles.submitButton}
+          mode="contained"
+          onPress={handleSubmit}
+        >
           Save Bid
         </Button>
       </TouchableOpacity>
