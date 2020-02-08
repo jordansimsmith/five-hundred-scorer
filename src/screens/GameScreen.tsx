df3bc1bc55e8391e597e2ecc5a893e83bfa8dcda
Jordan Sim-Smith <jordansimsmith@gmail.com>
2020-02-08 22:54:13 +1300

    Created basic game screen with dummy data

--- src/screens/GameScreen.tsx
+++ src/screens/GameScreen.tsx
@@ -1,6 +1,12 @@
 import React, { FunctionComponent } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { DataTable, Theme, withTheme } from 'react-native-paper';
+import { View, TouchableOpacity, StyleSheet } from 'react-native';
+import {
+  Button,
+  DataTable,
+  Subheading,
+  Theme,
+  withTheme,
+} from 'react-native-paper';
 
 interface IProps {
   theme: Theme;
@@ -9,17 +15,62 @@ interface IProps {
 }
 
 const GameScreen: FunctionComponent<IProps> = (props: IProps) => {
-  const { route } = props;
-
-  console.log(route.params);
+  const {
+    teamOnePlayerOne,
+    teamOnePlayerTwo,
+    teamTwoPlayerOne,
+    teamTwoPlayerTwo,
+  } = props.route.params;
 
   return (
-    <View>
-      <Text>Welcome to the game screen.</Text>
+    <View style={styles.container}>
+      <DataTable style={styles.dataTable}>
+        <DataTable.Header>
+          <DataTable.Title style={styles.tableCell}>
+            <Subheading>
+              {`${teamOnePlayerOne} & ${teamOnePlayerTwo}`}
+            </Subheading>
+          </DataTable.Title>
+          <DataTable.Title style={styles.tableCell}>
+            <Subheading>
+              {`${teamTwoPlayerOne} & ${teamTwoPlayerTwo}`}
+            </Subheading>
+          </DataTable.Title>
+        </DataTable.Header>
+
+        <DataTable.Row>
+          <DataTable.Cell style={styles.tableCell}>6 Spades</DataTable.Cell>
+          <DataTable.Cell style={styles.tableCell}>40</DataTable.Cell>
+          <DataTable.Cell style={styles.tableCell}></DataTable.Cell>
+          <DataTable.Cell style={styles.tableCell}>20</DataTable.Cell>
+        </DataTable.Row>
+      </DataTable>
+
+      <TouchableOpacity>
+        <Button style={styles.buttonContent} mode="contained">
+          New Bid
+        </Button>
+      </TouchableOpacity>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  dataTable: {
+    flex: 1,
+    justifyContent: 'flex-start',
+  },
+  tableCell: {
+    justifyContent: 'center',
+  },
+  buttonContent: {
+    height: 50,
+    justifyContent: 'center',
+  },
+});
 
 export default withTheme(GameScreen);
